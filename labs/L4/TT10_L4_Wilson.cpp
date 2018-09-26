9c169c311a7cfaf897f2b657bf895bd18e346a9a
Chandler Wilson <chandler9wilson@gmail.com>
2018-09-26 15:57:07 -0500

    Init lab 4

---
+++ labs/L4/TT10_L4_Wilson.cpp
@@ -0,0 +1,9 @@
+/* Author: Chandler Wilson
+Course: COSC 1337 Fall 2018
+Instructor: Thayer
+Lab 4:
+  1. Provide menu-driven option loop that:
+    * Asks a user for what medium they want to calc
+    * Input the number of feet through the medium
+    * Return how long that will take
+    * Accept material options from a file (up to 6) */
