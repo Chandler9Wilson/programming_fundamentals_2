4fdd64356dff47a039b9411eb975cc290819c9c6
Chandler Wilson <chandler9wilson@gmail.com>
2018-10-09 11:57:10 -0500

    init

--- exams/exam_1/question_scratch.cpp
+++ exams/exam_1/question_scratch.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+float total_pay(float hrs_worked = 40, float rate = 12.0)
+{
+  if (hrs_worked <= 40)
+  {
+    return hrs_worked * rate;
+  }
+  else
+  {
+    float total = rate * 40;
+    total += ((hrs_worked - 40) * rate * 1.5);
+    return total;
+  }
+}
+
 int main()
 {
   cout << 6 - 6 / 3 + 3 << endl;
@@ -30,4 +44,7 @@ int main()
   string s2 = "lioness";
 
   cout << (s1 == s2) << endl;
+
+  double payment = 142.954321;
+  cout << "The total pay is $" << fixed << setprecision(2) << payment;
 }
