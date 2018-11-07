2cd4a4630dfeba2b9b4343c11dae123ed709a671
Chandler Wilson <chandler9wilson@gmail.com>
2018-11-06 22:28:03 -0600

    Turned in lab 8

--- labs/L8/TT10_L8_Wilson.cpp
+++ labs/L8/TT10_L8_Wilson.cpp
@@ -3,9 +3,10 @@ Course: COSC 1337 Fall 2018
 Instructor: Thayer
 Lab 8:
   1. Tic Tac Toe game */
+#include <algorithm>
+#include <climits>
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
@@ -16,10 +17,23 @@ enum Player
   empty
 };
 
+char display_player(Player player_value)
+{
+  if (player_value == Player::x)
+  {
+    return 'X';
+  }
+  else if (player_value == Player::o)
+  {
+    return 'O';
+  }
+}
+
 class Board
 {
 private:
-  Player state[3][3];
+  const static int size_of_grid = 3;
+  Player state[size_of_grid][size_of_grid];
 
   /* Converts a plot number on 3x3 grid e.g.:
     7 | 8 | 9
@@ -32,38 +46,39 @@ private:
   {
     switch (plot_number)
     {
-    case 1:
-      column, row = 0;
+    case 7:
+      column = 0;
+      row = 0;
       return 0;
-    case 2:
+    case 8:
       column = 0;
       row = 1;
       return 0;
-    case 3:
+    case 9:
       column = 0;
       row = 2;
       return 0;
     case 4:
-      column = 0;
-      row = 1;
+      column = 1;
+      row = 0;
       return 0;
     case 5:
       column = 1;
       row = 1;
       return 0;
     case 6:
-      column = 2;
-      row = 1;
-      return 0;
-    case 7:
-      column = 0;
-      row = 2;
-      return 0;
-    case 8:
       column = 1;
       row = 2;
       return 0;
-    case 9:
+    case 1:
+      column = 2;
+      row = 0;
+      return 0;
+    case 2:
+      column = 2;
+      row = 1;
+      return 0;
+    case 3:
       column = 2;
       row = 2;
       return 0;
@@ -76,6 +91,14 @@ private:
 public:
   Board()
   {
+    // sets every plot to empty state
+    for (int column = 0; column < size_of_grid; column++)
+    {
+      for (int row = 0; row < size_of_grid; row++)
+      {
+        state[column][row] = Player::empty;
+      }
+    }
   }
 
   int move_by_coordinate(int column, int row, Player player)
@@ -119,9 +142,145 @@ public:
       return 1;
     }
   }
+
+  void get_state(Player return_state[size_of_grid][size_of_grid])
+  {
+    copy(&state[0][0], &state[0][0] + size_of_grid * size_of_grid, &return_state[0][0]);
+  }
 };
+
+void display_board(Player state_of_board[3][3])
+{
+  int num_pad[3][3] =
+      {
+          {7, 8, 9},
+          {4, 5, 6},
+          {1, 2, 3}};
+
+  for (int column = 0; column < 3; column++)
+  {
+    for (int row = 0; row < 3; row++)
+    {
+      if (state_of_board[column][row] != Player::empty)
+      {
+        cout << display_player(state_of_board[column][row]);
+      }
+      else
+      {
+        cout << num_pad[column][row];
+      }
+
+      if (row == 2)
+      {
+        if (column == 2)
+        {
+          cout << endl;
+
+          continue;
+        }
+        else
+        {
+          cout << endl
+               << string(9, '-') << endl;
+
+          continue;
+        }
+      }
+
+      cout << " | ";
+    }
+  }
+}
+
+int get_input(const string &prompt = "enter value: ",
+              const string &error_message = "invalid value, try again.")
+{
+  while (true)
+  {
+    cout << prompt;
+
+    int value{0};
+
+    if (cin >> value and value >= 1 and value <= 9)
+    {
+      return value;
+    }
+    else
+    {
+      cout << "  " << error_message << endl;
+      cin.clear();
+      cin.ignore(INT_MAX, '\n');
+    }
+  }
+}
 
 int main()
 {
-  Board test;
+  Board player_board;
+
+  string prompt_X = "Player X enter the position you would like to move: ";
+  string prompt_O = "Player O enter the position you would like to move: ";
+  string error_message = "Please enter an unused number between 1 and 9.";
+  int player_turn = 2;
+
+  while (true)
+  {
+    Player board_state[3][3];
+    player_board.get_state(board_state);
+
+    display_board(board_state);
+
+    if (player_turn % 2)
+    {
+      int plot_number = get_input(prompt_O, error_message);
+      player_board.move_by_plot_number(plot_number, Player::o);
+    }
+    else
+    {
+      int plot_number = get_input(prompt_X, error_message);
+      player_board.move_by_plot_number(plot_number, Player::x);
+    }
+
+    player_turn++;
+  }
 }
+
+/* Testing:
+7 | 8 | 9
+---------
+4 | 5 | 6
+---------
+1 | 2 | 3
+Player X enter the position you would like to move: 1
+7 | 8 | 9
+---------
+4 | 5 | 6
+---------
+X | 2 | 3
+Player O enter the position you would like to move: 2
+7 | 8 | 9
+---------
+4 | 5 | 6
+---------
+X | O | 3
+Player X enter the position you would like to move: 5
+7 | 8 | 9
+---------
+4 | X | 6
+---------
+X | O | 3
+Player O enter the position you would like to move: 9\
+7 | 8 | O
+---------
+4 | X | 6
+---------
+X | O | 3
+Player X enter the position you would like to move:   Please enter an unused number between 1 and 9.
+Player X enter the position you would like to move: 9
+7 | 8 | X
+---------
+4 | X | 6
+---------
+X | O | 3
+Player O enter the position you would like to move:
+ */
