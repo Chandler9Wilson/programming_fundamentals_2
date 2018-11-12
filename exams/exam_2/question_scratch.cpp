6f64c6bacec29d4e62841582a47cab3a62086d4d
Chandler Wilson <chandler9wilson@gmail.com>
2018-11-11 18:00:59 -0600

    init exam 2

---
+++ exams/exam_2/question_scratch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+class Test
+{
+private:
+  double shit;
+
+public:
+  double hmmm;
+
+private:
+  double huhhh;
+
+public:
+  double wadu;
+};
+
+main()
+{
+  double money[25];
+}
