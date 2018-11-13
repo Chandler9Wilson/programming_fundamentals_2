3f68eb8ebe38e8b1a61ae080b60eef6af78f9a65
Chandler Wilson <chandler9wilson@gmail.com>
2018-11-13 12:25:46 -0600

    Turned in lab

--- exams/exam_2/TT10_X2_Wilson.cpp
+++ exams/exam_2/TT10_X2_Wilson.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <iomanip>
 #include <limits>
+#include <string>
 using namespace std;
 
 /* INSTRUCTIONS: (OK to remove this big comment block, but keep smaller // comments.)
@@ -116,56 +117,74 @@ public:
 
   // Step 6) avoid redundant code, call set() in constructor(s)
 
-  CoinPurse()
+  CoinPurse(int new_quarters = 0, int new_dimes = 0, int new_nickels = 0, int new_pennies = 0)
   {
-    quarters, dimes, nickels, pennies = 0;
-  }
-
-  CoinPurse(int, int, int, int)
-  {
+    if (set(new_quarters, new_dimes, new_nickels, new_pennies))
+    {
+    }
+    else
+    {
+      set(0, 0, 0, 0);
+    }
   }
 
   // Step 3) Write total_value to return total value of coins in CoinPurse object
-  int total_value() const; // return total cents as int // getters should be marked as const
+  int total_value() const
+  {
+    return ((quarters * 25) + (dimes * 10) + (nickels * 5) + (pennies));
+  }
 
   // Step 5) Implement set() which modifies the coin counts
-  bool set(int, int, int, int);
+  bool set(int new_quarters, int new_dimes, int new_nickels, int new_pennies)
+  {
+    /* Step 5. (5 points)
+    In class CoinPurse, implement a mutator/setter called set() which sets the
+    coin counts of all coins. Pre-validate any new coin counts; do not allow any
+    coin count to be less than zero. If the ANY of the coin counts are invalid
+    (negative), do not modify ANY coin counts. If the set() succeeds, return true;
+    if the set() fails, return false. */
+    if (new_quarters >= 0 and new_dimes >= 0 and new_nickels >= 0 and new_pennies >= 0)
+    {
+      quarters = new_quarters;
+      dimes = new_dimes;
+      nickels = new_nickels;
+      pennies = new_pennies;
+
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
 
   // Step 7) write show() method that outputs the number of each coin in the purse
   //  in one string, like this: "q=4 d=3 n=2 p=1" (don't << endl;)
-  void show() const;
+  string show() const
+  {
+    return ("(q=" + to_string(quarters) + " d=" + to_string(dimes) + " n=" +
+            to_string(nickels) + " p=" + to_string(pennies) + ")");
+  }
 
   // Step 9) write modify() method to modify coin counts using a positive (increment),
   // negative (decrement) or zero (keep the same) value.
-  bool modify(int, int, int, int);
+  bool modify(int modify_quarters, int modify_dimes, int modify_nickels, int modify_pennies)
+  {
+    int new_quarters = quarters + modify_quarters;
+    int new_dimes = dimes + modify_dimes;
+    int new_nickels = nickels + modify_nickels;
+    int new_pennies = pennies + modify_pennies;
+
+    if (set(new_quarters, new_dimes, new_nickels, new_pennies))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
 };
-
-template <typename T> // This is provided. Use it to get values from the user.
-T get_value(const string &prompt = "enter value: ",
-            const string &errmsg = "  invalid, try again\n",
-            T value_min = -numeric_limits<T>::max(), T value_max = numeric_limits<T>::max());
-// Hint: char option = get_value<char>("Modify p)enny n)ickel d)ime q)uarter s)top: ");
-
-int main()
-{
-  cout << "COSC 1337 Exam 2 CoinPurse" << endl;
-
-  // Step 2) declare CoinPurse object called purse;
-  // initialize with: 4 quarters, 3 dimes, 2 nickels, 1 penny
-
-  // Step 4) Call the total_value method on purse; display the result formatted as: $x.xx
-
-  // Step 8) Call set to change values in purse to: 8 quarters, 7 dimes, 6 nickels, 5 pennies.
-  //         Call show to display the contents of purse
-
-  // Step 10) write menu driven loop that allows the user the add or remove coins from purse.
-
-  // Step 12 EXTRA CREDIT) add c)ents option that allows the user to add a specific amount of cents
-
-  cout << endl
-       << "Goodbye!" << endl; // this should appear as the last line of your output
-  return EXIT_SUCCESS;        // Use return 0 if EXIT_SUCCESS is undefined
-}
 
 template <typename T>
 T get_value(const string &prompt = "enter value: ",
@@ -188,7 +207,76 @@ T get_value(const string &prompt = "enter value: ",
     }
   }
 }
+// Hint: char option = get_value<char>("Modify p)enny n)ickel d)ime q)uarter s)top: ");
+
+int main()
+{
+  cout << "COSC 1337 Exam 2 CoinPurse" << endl;
+
+  // Step 2) declare CoinPurse object called purse;
+  // initialize with: 4 quarters, 3 dimes, 2 nickels, 1 penny
+  CoinPurse my_purse(4, 3, 2, 1);
+
+  // Step 4) Call the total_value method on purse; display the result formatted as: $x.xx
+  cout << "The total purse value is $" << fixed << setprecision(2) << (my_purse.total_value() * .01) << " ";
+
+  // Step 8) Call set to change values in purse to: 8 quarters, 7 dimes, 6 nickels, 5 pennies.
+  my_purse.set(8, 7, 6, 5);
+  //         Call show to display the contents of purse
+  cout << my_purse.show() << endl;
+
+  // Step 10) write menu driven loop that allows the user the add or remove coins from purse.
+  while (true)
+  {
+    // $3.05 (q=8 d=7 n=6 p=5) Modify p)enny n)ickel d)ime q)uarter s)top: p -3
+    cout << "$" << fixed << setprecision(2) << (my_purse.total_value() * .01) << " " << my_purse.show() << " ";
+    char option = get_value<char>("Modify p)enny n)ickel d)ime q)uarter s)top: ");
+    int modify_by{0};
+
+    if (option == 's')
+    {
+      break;
+    }
+    else
+    {
+      modify_by = get_value<int>("");
+    }
+
+    switch (option)
+    {
+    case 'q':
+      my_purse.modify(modify_by, 0, 0, 0);
+      break;
+    case 'd':
+      my_purse.modify(0, modify_by, 0, 0);
+      break;
+    case 'n':
+      my_purse.modify(0, 0, modify_by, 0);
+      break;
+    case 'p':
+      my_purse.modify(0, 0, 0, modify_by);
+      break;
+    default:
+      cout << "Invalid option. Try again" << endl;
+      continue;
+    }
+  }
+
+  // Step 12 EXTRA CREDIT) add c)ents option that allows the user to add a specific amount of cents
+
+  cout << endl
+       << "Goodbye!" << endl; // this should appear as the last line of your output
+  return EXIT_SUCCESS;        // Use return 0 if EXIT_SUCCESS is undefined
+}
 
 /*  Step 11) Paste test output here (do this after step 12 if doing extra credit)
+COSC 1337 Exam 2 CoinPurse
+The total purse value is $1.41 (q=8 d=7 n=6 p=5)
+$3.05 (q=8 d=7 n=6 p=5) Modify p)enny n)ickel d)ime q)uarter s)top: p -3
+$3.02 (q=8 d=7 n=6 p=2) Modify p)enny n)ickel d)ime q)uarter s)top: n 0
+$3.02 (q=8 d=7 n=6 p=2) Modify p)enny n)ickel d)ime q)uarter s)top: d 2
+$3.22 (q=8 d=9 n=6 p=2) Modify p)enny n)ickel d)ime q)uarter s)top: q -4
+$2.22 (q=4 d=9 n=6 p=2) Modify p)enny n)ickel d)ime q)uarter s)top: s
 
+Goodbye!
 */
